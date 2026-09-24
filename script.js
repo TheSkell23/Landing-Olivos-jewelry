a5ed6923132443a678f738007c8a3384534392e5
TheSkell23 <fuentes.diego.1969@gmail.com>
2026-09-24 17:09:25 -0400

    Initial commit: Cafe Luna landing

---
+++ script.js
@@ -0,0 +1,8 @@
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add('visible');
+    }
+  });
+});
+document.querySelectorAll('.section').forEach(el => observer.observe(el));
